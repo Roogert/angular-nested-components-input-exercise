aadebcd87a2b9e112f721de1b186bfb375771e86
Roogert <samrellergert@gmail.com>
2022-03-31 20:02:02 -0500

    class exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-nested-components-input-exercise';
+  myPosts: string[] =["Scooby", "Doo", "Where", "Are", "You"]
+  
 }
